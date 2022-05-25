b7170c405898d0a577e9c23ce064293eded084d5
AS2005071645\Пользователь <zytia@mail.ru>
2022-05-25 17:58:13 +0300

    removed hardcode

--- app/Enum/RolesEnum.php
+++ app/Enum/RolesEnum.php
@@ -1,9 +1,27 @@
 <?php
 namespace App\Enum;
 
+/**
+ *
+ * Class RolesEnum
+ * Класс с ролями в системе
+ *
+ * @package App\Enum
+ */
 abstract class RolesEnum
 {
+    /**
+     * Преподаватель
+     */
     const professor = "PREPOD";
+
+    /**
+     * Студент
+     */
     const student = "STUDENT";
+
+    /**
+     * Администратор
+     */
     const admin = "ADMIN";
 }
---
+++ app/Enum/TimeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Enum;
+
+
+abstract class TimeEnum
+{
+    const day = "day";
+    const week = "week";
+    const month = "month";
+}
